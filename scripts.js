936e854bf60d01da6a788229dcab84795efd0078
edu-silvadev <edusilvalc3@gmail.com>
2026-04-06 20:37:58 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 function calcular() {
     //nJS é uma linguagem não tipada (sem tipo de dados) 
     // Number() é uma função que convert texto em número
+
+    // recuperta os valores 
     // reacupera o valor informado no elemento com id qtdeArroz
     let qtdeArroz = Number(document.getElementById("qtdeArroz").value)
     let qtdeFeijao = Number(document.getElementById("qtdeFeijao").value)
@@ -10,12 +12,30 @@ function calcular() {
     let pontos = (qtdeArroz * 10) + (qtdeFeijao * 8) + (qtdeOleo * 4) + (qtdeMacarrao * 2)
 
     // calcula o valor total das metas
+    // recupera a cor da equipe
+    let cor = document.getElementById("cor").value
+    let metaProva1 //declara  a variável
+    if (cor == "amarela") {
+        metaProva1 = 54 
+    }
+    else if (cor == "cinza") {
+        metaProva1 = 51
+    }
+    else if (cor == "laranja") {
+        metaProva1 = 21
+    }
+    else if (cor == "marrom") {
+        metaProva1 = 88
+    }
+    else if (cor == "preta") {
+        metaProva1 = 60
+    }
     let kitAlimentacao = Number(document.getElementById("kitAlimentacao").value)
-    if (kitAlimentacao >= 60) {
+    if (kitAlimentacao >= metaProva1) {
         pontos = pontos + 5000 // 5000 garantidos 
         if (kitAlimentacao > 60) {
             // ganhamos 83.33 a mais de ponto por itens  que utrapassaram 60 
-            pontos = pontos + (kitAlimentacao-60) * 83.33 // 5000/60(Alunos)=83.33
+            pontos = pontos + (kitAlimentacao-metaProva1) * (5000/metaProva1) // 5000/60(Alunos)=83.33
         }
     }
     else {
