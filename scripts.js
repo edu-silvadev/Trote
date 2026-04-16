bf59fde826ac423073a12f1a57076666a41669e6
edu-silvadev <edusilvalc3@gmail.com>
2026-04-15 21:52:13 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,47 +1,40 @@
-function calcular() {
-    //nJS é uma linguagem não tipada (sem tipo de dados) 
-    // Number() é uma função que convert texto em número
-
-    // recuperta os valores 
-    // reacupera o valor informado no elemento com id qtdeArroz
-    let qtdeArroz = Number(document.getElementById("qtdeArroz").value)
-    let qtdeFeijao = Number(document.getElementById("qtdeFeijao").value)
-    let qtdeOleo = Number(document.getElementById("qtdeOleo").value)
-    let qtdeMacarrao = Number(document.getElementById("qtdeMacarrao").value)
-    // calcular o valor total de pontos dos itens avulsos 
-    let pontos = (qtdeArroz * 10) + (qtdeFeijao * 8) + (qtdeOleo * 4) + (qtdeMacarrao * 2)
-
-    // calcula o valor total das metas
-    // recupera a cor da equipe
-    let cor = document.getElementById("cor").value
-    let metaProva1 //declara  a variável
-    if (cor == "amarela") {
-        metaProva1 = 54 
-    }
-    else if (cor == "cinza") {
-        metaProva1 = 51
-    }
-    else if (cor == "laranja") {
-        metaProva1 = 21
-    }
-    else if (cor == "marrom") {
-        metaProva1 = 88
-    }
-    else if (cor == "preta") {
-        metaProva1 = 60
-    }
-    let kitAlimentacao = Number(document.getElementById("kitAlimentacao").value)
-    if (kitAlimentacao >= metaProva1) {
-        pontos = pontos + 5000 // 5000 garantidos 
-        if (kitAlimentacao > 60) {
-            // ganhamos 83.33 a mais de ponto por itens  que utrapassaram 60 
-            pontos = pontos + (kitAlimentacao-metaProva1) * (5000/metaProva1) // 5000/60(Alunos)=83.33
-        }
+function churras(){
+    // recupera os dados do usuário
+    let convidados = Number(document.getElementById("convidados").value)
+    let total = convidados * 400 // em gramas
+    let totalKg = total / 1000 // em kilos
+    document.getElementById("resultadoCarne").innerText = 
+            "Total em kg " + totalKg.toFixed(2)
+}
+function febre(){
+    let temperatura = Number(document.getElementById("temperatura").value)
+    if (temperatura >= 37.5){
+        document.getElementById("resultadoFebre").innerText = "Está com febre"
     }
     else {
-        pontos = pontos + kitAlimentacao * 83.33
+        document.getElementById("resultadoFebre").innerText = "Temperatura normal"
     }
-
-    //vamos exibir o resultado para o usuário
-    document.getElementById("resultado").innerText = "Pontuação total: " + pontos + " pontos"
+}
+function emprestimo(){
+    let renda = Number(document.getElementById("renda").value)
+    let idade = Number(document.getElementById("idade").value)
+    if (renda > 2000 && idade > 18){
+        document.getElementById("resultadoEmprestimo").innerText = "Empréstimo aprovado"
+    }
+    else {
+        document.getElementById("resultadoEmprestimo").innerText = "Empréstimo reprovado"
+    }
+}
+function luta(){
+    let peso = Number(document.getElementById("peso").value)
+    let idade = Number(document.getElementById("idadeLuta").value)
+    if (idadeLuta < 18){
+        document.getElementById("resultadoCategoria").innerText = "Categoria Juvenil"
+    }
+    else if (peso < 80){
+        document.getElementById("resultadoCategoria").innerText = "Categoria adulto leve"
+    }
+    else {
+        document.getElementById("resultadoCategoria").innerText = "Categoria adulto pesado"
+    }
 }
